7be04766e72953487a8b0c71e0581bea0bd5e165
Antrea Constantinou <antrea.constantinou.2019@uni.strath.ac.uk>
2025-03-10 13:01:01 +0000

    test commit

--- GitTest.py
+++ GitTest.py
@@ -1,1 +1,8 @@
-print("Test Git Commit")
+print("Git commands:")
+
+print("Stage changes git add - git add .")
+print("git commit -m  'commit msg' ")
+print("git push -u origin main")
+print("git pull")
+print("git status")
+print("git log")
