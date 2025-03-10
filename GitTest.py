f8639cb69a46f7641da310aa40917954f4e5090e
Antrea Constantinou <antrea.constantinou.2019@uni.strath.ac.uk>
2025-03-10 13:13:33 +0000

    update git commands

--- GitTest.py
+++ GitTest.py
@@ -1,8 +1,11 @@
 print("Git commands:")
 
-print("Stage changes git add - git add .")
-print("git commit -m  'commit msg' ")
-print("git push -u origin main")
+print("1. Stage changes git add - git add . / to stage a specific file - git add file_name")
+print("2. git commit -m 'commit msg'")
+print("3. git push -u origin current_branch")
+
+print("delete file - git rm file_name")
+print("switch branch - git checkout branch_name")
 print("git pull")
 print("git status")
 print("git log")
