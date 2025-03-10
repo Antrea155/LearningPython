76d4e42ade62d9bd7e2e5a0551ebf908ad6fdb80
Antrea Constantinou <antrea.constantinou.2019@uni.strath.ac.uk>
2025-03-10 13:08:23 +0000

    Remove files from main

--- GitTest.py
+++
@@ -1,8 +0,0 @@
-print("Git commands:")
-
-print("Stage changes git add - git add .")
-print("git commit -m  'commit msg' ")
-print("git push -u origin main")
-print("git pull")
-print("git status")
-print("git log")
